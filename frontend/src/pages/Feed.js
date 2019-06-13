21f58fe64c1d89327dcae19ea948c7bdfc5b1ddb
Marcos de P. Muniz <marcospmuniz@gmail.com>
2019-06-13 12:07:37 -0300

    finalização do frontend com atualização real-time

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -2,6 +2,7 @@
 
 import React, { Component } from 'react';
 import api from '../services/api';
+import io from 'socket.io-client';
 
 import './Feed.css';
 
@@ -12,13 +13,54 @@ import send from '../assets/send.svg';
 
 class Feed extends Component {
 
-    state = {
-        feed: []
-    };
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            feed: []
+        }
+
+        this.handleLike = this.handleLike.bind(this);
+        this.registerToSocket = this.registerToSocket.bind(this);
+
+    }
 
     async componentDidMount() {
+
+        // registra o websocket para atualização real-time
+        this.registerToSocket();
+
         const response = await api.get('/posts');
         this.setState({ feed: response.data });
+    }
+
+    registerToSocket = function() {
+        const socket = io('http://localhost:3333');
+
+        // post, like
+
+        socket.on('post', function(newPost) {
+            // aqui eu pego o novo post e coloco ele no inicio feed já existente 
+            // no this.state.feed e seto o valor do state.feed agora com o novo post no inicio
+            this.setState({ feed: [newPost, ...this.state.feed] });
+        });
+
+        socket.on('like', likedPost => {
+            // aqui, para atualizar a quantidade de likes de uma postagem que recebeu um novo
+            // like em tempo real eu precorro todas as postagens do feed procurando pela postagem
+            // com o _id da postagem que recebeu o like, quando encontro, eu atualizo o state dessa
+            // postagem especifica pelo valor da postagem que recebeu o like para alterar na tela 
+            // a quantidade de likes dela.
+            this.setState({
+                feed: this.state.feed.map(post => {
+                    return post._id === likedPost._id ? likedPost : post
+                })
+            });
+        });
+    }
+
+    handleLike = function(id) {
+        api.post(`/posts/${id}/like`);
     }
 
     render() {
@@ -42,7 +84,12 @@ class Feed extends Component {
 
                             <footer>
                                 <div className="actions">
-                                    <img src={like} alt="Curtir" />
+                                    {/* Para conseguir passar parametro em função (this.handleLike(post._id)) se que a
+                                    função seja executada ao ser renderizada, basta passar a função como retorno
+                                    de outra função, como no exemplo abaixo onde isso é feito usando arrow function */}
+                                    <button type="button" onClick={() => this.handleLike(post._id) }>
+                                        <img src={like} alt="Curtir" />
+                                    </button>
                                     <img src={comment} alt="Comentar" />
                                     <img src={send} alt="Enviar" />
                                 </div>
--- frontend/src/pages/New.js
+++ frontend/src/pages/New.js
@@ -1,10 +1,95 @@
 import React, { Component } from 'react';
+import api from '../services/api';
+
+import './New.css';
 
 class New extends Component {
 
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            image: null,
+            author: '',
+            place: '',
+            description: '',
+            hashtags: ''
+        }
+
+        this.handleImageChange = this.handleImageChange.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+
+    handleImageChange = function(e) {
+        this.setState({ image: e.target.files[0] });
+    }
+
+    handleChange = function(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    handleSubmit = async function(e) {
+        e.preventDefault();
+        
+        // como este formulário é enviado para api como um multipart-formdata, ele precisa
+        // ser enviado da forma abaixo, e não como um json puro
+        const data = new FormData();
+
+        data.append('image', this.state.image);
+        data.append('author', this.state.author);
+        data.append('place', this.state.place);
+        data.append('description', this.state.description);
+        data.append('hashtags', this.state.hashtags);
+
+        await api.post('posts', data);
+
+        // redireciona o acesso para a rota /
+        this.props.history.push('/');
+
+    }
+
     render() {
         return (
-            <h1>New</h1>
+            <form id="new-post" onSubmit={this.handleSubmit}>
+                <input type="file" onChange={this.handleImageChange} />
+
+                <input 
+                type="text" 
+                name="author"
+                placeholder="Autor do post"
+                onChange={this.handleChange}
+                value={this.state.author}
+                />
+
+                <input 
+                type="text" 
+                name="place"
+                placeholder="Local do post"
+                onChange={this.handleChange}
+                value={this.state.place}
+                />
+
+                <input 
+                type="text" 
+                name="description"
+                placeholder="Descrição do post"
+                onChange={this.handleChange}
+                value={this.state.description}
+                />
+
+                <input 
+                type="text" 
+                name="hashtags"
+                placeholder="Hashtags do post"
+                onChange={this.handleChange}
+                value={this.state.hashtags}
+                />
+
+                <button type="submit">Enviar</button>
+            </form>
         );
     }
 
